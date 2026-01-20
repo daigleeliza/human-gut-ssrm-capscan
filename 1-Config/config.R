0a1df16f87214f8dc1a68edb45a729cee663764a
Eliza Daigle <elizad46@gmail.com>
2026-01-20 14:56:16 -0500

    commit

--- 1-Config/config.R
+++ 1-Config/config.R
@@ -11,5 +11,8 @@ library()
 #--------------------------------------------
 # define repository paths
 #--------------------------------------------
+configDir = paste0(here::here(), "/0-Cluster/config/")
 treeInfoDir = paste0(here::here(), "/0-Cluster/workflow/out/treeInfo/")
 abundanceDir = paste0(here::here(), "/0-Cluster/workflow/out/dsrAB_CapsuleStool_Abundances/")
+
+dataDir = paste0(here::here(), "/data/")

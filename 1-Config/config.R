7f3dd2a33d5a57655603c2a256e90f930018162c
Eliza Daigle <elizad46@gmail.com>
2026-01-20 14:16:23 -0500

    moving filtering steps to a data management folder

--- 1-Config/config.R
+++ 1-Config/config.R
@@ -6,3 +6,10 @@
 # source base functions, if needed
 #######################################
 library()
+
+
+#--------------------------------------------
+# define repository paths
+#--------------------------------------------
+treeInfoDir = paste0(here::here(), "/0-Cluster/workflow/out/treeInfo/")
+abundanceDir = paste0(here::here(), "/0-Cluster/workflow/out/dsrAB_CapsuleStool_Abundances/")

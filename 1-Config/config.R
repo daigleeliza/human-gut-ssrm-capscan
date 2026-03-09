aeb7e403452186000a2f9f206982d9e23bef04fa
Eliza Daigle <elizad46@gmail.com>
2026-03-09 12:36:43 -0400

    reading asr file in from the cluster

--- 1-Config/config.R
+++ 1-Config/config.R
@@ -65,5 +65,6 @@ configDir = paste0(here::here(), "/0-Cluster/config/")
 treeInfoDir = paste0(here::here(), "/0-Cluster/workflow/out/treeInfo/")
 raxmlDir = paste0(here::here(), "/0-Cluster/workflow/out/raxmlOutput")
 abundanceDir = paste0(here::here(), "/0-Cluster/workflow/out/dsrAB_CapsuleStool_Abundances/")
+abundanceDirASR = paste0(here::here(), "/0-Cluster/workflow/out/asrABC_CapsuleStool_Abundances/")
 
 dataDir = paste0(here::here(), "/data/")
